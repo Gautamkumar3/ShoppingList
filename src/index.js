59d623763fd69a51b9aadd956f9c73774123f573
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-12-15 11:43:26 +0530

    basic structure add

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./config/db.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+mongoose.set("strictQuery", false);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Wecome");
+});
+
+app.listen(PORT, async () => {
+  await dbConnect();
+  console.log("Server is running on port 8080");
+});
